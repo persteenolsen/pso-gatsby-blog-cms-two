31b63663b5bfa3632fa357e4e7049b813a754586
Per <38952136+persteenolsen@users.noreply.github.com>
2022-06-19 09:14:02 +0200

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -30,15 +30,15 @@ const About = () => {
            	<b>The purpose of this Blog</b>
 			<ul>
             <li>To get experience with Gatsby React GraphQL and Git-based Headless CMS systems</li>
-            <li>Comparing different technologies</li>
+            <li>Comparing different technologies with focus on login systems</li>
             <li>Showing the Web Projects I am building by a fast Blog</li>
-			<li>Help other developers building Web Projects by fork or download from my GitHub</li>
+	    <li>Help other developers building Web Projects by fork or download from my GitHub</li>
             <li>Let the readers of the Blog get to know me as well as my IT background</li>
             </ul>
                         
             <b>The static part of the Blog was last updated</b>
             <ul>
-			<li>22-05-2022</li>
+			<li>19-06-2022</li>
 			</ul>
 			
 
