a5b7a2becf41cbda4a866cbf8536bf75d6c03249
Per <38952136+persteenolsen@users.noreply.github.com>
2022-07-13 12:30:14 +0200

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -38,7 +38,7 @@ const About = () => {
                         
             <b>The static part of the Blog was last updated</b>
             <ul>
-			<li>06-07-2022</li>
+			<li>13-07-2022</li>
 			</ul>
 			
 
