36bcfe73ad75abaa08c67f7536c7bf4a65b8ab07
Per Steen Olsen <persteen22@hotmail.com>
2023-06-01 12:20:22 +0200

    Tested the DevOps flow andd upated the About Page

--- src/pages/about.js
+++ src/pages/about.js
@@ -38,7 +38,7 @@ const About = () => {
 
             <b>The static part of the Blog was last updated</b>
             <ul>
-                <li>30-05-2023</li>
+                <li>01-06-2023</li>
             </ul>
 
 
