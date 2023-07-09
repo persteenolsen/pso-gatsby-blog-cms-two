d7eb2ed6b5169e1cc9696198f99f759d2f47643f
Per Steen Olsen <persteen22@hotmail.com>
2023-07-09 12:26:59 +0200

    The About Page was upated + tested automated build

--- src/pages/about.js
+++ src/pages/about.js
@@ -38,7 +38,7 @@ const About = () => {
 
             <b>The static part of the Blog was last updated</b>
             <ul>
-                <li>06-07-2023</li>
+                <li>09-07-2023</li>
             </ul>
 
 
