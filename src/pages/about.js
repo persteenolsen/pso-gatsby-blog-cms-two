5c4706e0954582646da3730ec3e149d771207ee3
Per Steen Olsen <persteen22@hotmail.com>
2023-07-18 14:39:45 +0200

    Updated a Post about Next.js with TypeScript

--- src/pages/about.js
+++ src/pages/about.js
@@ -38,7 +38,7 @@ const About = () => {
 
             <b>The static part of the Blog was last updated</b>
             <ul>
-                <li>17-07-2023</li>
+                <li>18-07-2023</li>
             </ul>
 
 
