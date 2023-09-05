8b83c09680706c70e0ac2cd5d189b4a2d33f2185
Per Steen Olsen <persteen22@hotmail.com>
2023-09-05 15:06:37 +0200

    Tested DevOps + Updated About Page

--- src/pages/about.js
+++ src/pages/about.js
@@ -38,7 +38,7 @@ const About = () => {
 
             <b>The static part of the Blog was last updated:</b>
             <ul>
-                <li>04-09-2023</li>
+                <li>05-09-2023</li>
             </ul>
 
 
