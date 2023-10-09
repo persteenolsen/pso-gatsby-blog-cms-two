10167c2db7bd8bb9a1d238c2fe1815fd0c1b9505
Per Steen Olsen <persteen22@hotmail.com>
2023-10-09 15:23:53 +0200

    Updated Post - Webpack 5 with ES6 + About Page

--- src/pages/about.js
+++ src/pages/about.js
@@ -38,7 +38,7 @@ const About = () => {
 
             <b>The static part of the Blog was last updated:</b>
             <ul>
-                <li>07-10-2023</li>
+                <li>09-10-2023</li>
             </ul>
 
 
