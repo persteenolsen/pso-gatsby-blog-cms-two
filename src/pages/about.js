5f5e8ebcb4017c15644822ebe5eedd4a61dd689b
Per Olsen <persteen22@gmail.com>
2024-12-11 08:48:58 +0100

    Updated the About Page

--- src/pages/about.js
+++ src/pages/about.js
@@ -38,7 +38,7 @@ const About = () => {
 
             <b>The static part of the Blog was last updated</b>
             <ul>
-                <li>10-12-2024</li>
+                <li>11-12-2024</li>
             </ul>
 
 
