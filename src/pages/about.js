eecdd4eb4a789cf1c30bd59a77af4e59e1a3aba6
Per Olsen <persteen22@gmail.com>
2025-03-05 11:39:23 +0100

    Updated Post - Git based CMS

--- src/pages/about.js
+++ src/pages/about.js
@@ -38,7 +38,7 @@ const About = () => {
 
             <b>The static part of the Blog was last updated:</b>
             <ul>
-                <li>04-03-2025</li>
+                <li>05-03-2025</li>
             </ul>
 
 
