ea2a29217321da6c11287da37a39f024fc549434
Per Olsen <persteen22@gmail.com>
2025-05-01 07:23:11 +0200

    Updated my CV and About Page

--- src/pages/about.js
+++ src/pages/about.js
@@ -38,7 +38,7 @@ const About = () => {
 
             <b>The static part of the Blog was last updated:</b>
             <ul>
-                <li>30-04-2025</li>
+                <li>01-05-2025</li>
             </ul>
 
 
