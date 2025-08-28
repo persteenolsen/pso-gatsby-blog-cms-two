25cb2d49a3cec8b0f09225aff06ee544db033f16
Per Olsen <persteen22@gmail.com>
2025-08-28 08:08:31 +0200

    Updated Post - Django + MySQL + Models with CRUD

--- src/pages/about.js
+++ src/pages/about.js
@@ -38,7 +38,7 @@ const About = () => {
 
             <b>The static part of the Blog was last updated:</b>
             <ul>
-                <li>27-08-2025</li>
+                <li>28-08-2025</li>
             </ul>
 
 
--- src/posts/2025-04-27-python-django-starter-two.md
+++ src/posts/2025-04-27-python-django-starter-two.md
@@ -1,6 +1,6 @@
 ---
 title: Python and Django Starter Website using MySQL serving a Blog Employees and Todos
-date: 2025-08-27
+date: 2025-08-28
 published: true
 categories:
   - Python
