6c84423aa82a6fa57af40497e7948db34364171e
Per Olsen <persteen22@gmail.com>
2025-09-16 08:34:22 +0200

    Updated Post - .NET 8 Basic Auth Blazor + Vue

--- src/pages/about.js
+++ src/pages/about.js
@@ -38,7 +38,7 @@ const About = () => {
 
             <b>The static part of the Blog was last updated:</b>
             <ul>
-                <li>15-09-2025</li>
+                <li>16-09-2025</li>
             </ul>
 
 
