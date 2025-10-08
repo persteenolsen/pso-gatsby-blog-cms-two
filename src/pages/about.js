b4670dbdea012b3dc154a3c4356b49110029809b
Per Olsen <persteen22@gmail.com>
2025-10-08 11:51:52 +0200

    Updated Post - Java Spring Boot + MVC + Security

--- src/pages/about.js
+++ src/pages/about.js
@@ -38,7 +38,7 @@ const About = () => {
 
             <b>The static part of the Blog was last updated:</b>
             <ul>
-                <li>07-10-2025</li>
+                <li>08-10-2025</li>
             </ul>
 
 
