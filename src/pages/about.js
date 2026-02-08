06cdbb986a7408396c391bf920c2a1262d8523d6
Per Olsen <persteen22@gmail.com>
2026-02-08 12:42:50 +0100

    Updated Posts - Wagtail CMS + Umbraco CMS

--- src/pages/about.js
+++ src/pages/about.js
@@ -38,7 +38,7 @@ const About = () => {
 
             <b>The static part of the Blog was last updated</b>
             <ul>
-                <li>07-02-2026</li>
+                <li>08-02-2026</li>
             </ul>
 
 
--- src/posts/2025-11-01-python-django-wagtail-pso.md
+++ src/posts/2025-11-01-python-django-wagtail-pso.md
@@ -1,6 +1,6 @@
 ---
 title: Python Django and Wagtail Website using PostgreSQL hosted at Vercel serving a Demo
-date: 2026-01-04
+date: 2026-02-08
 published: true
 categories:
   - Python
