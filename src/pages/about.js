33af20e5010e4826eb9ecc3e9bcfd2829194f85b
Per Olsen <persteen22@gmail.com>
2026-02-26 09:08:54 +0100

    Updated Post - Python Django PythonAnywhere

--- src/pages/about.js
+++ src/pages/about.js
@@ -38,7 +38,7 @@ const About = () => {
 
             <b>The static part of the Blog was last updated</b>
             <ul>
-                <li>25-02-2026</li>
+                <li>26-02-2026</li>
             </ul>
 
 
--- src/posts/2025-10-19-python-django-pythonanywhere.md
+++ src/posts/2025-10-19-python-django-pythonanywhere.md
@@ -1,6 +1,6 @@
 ---
 title: Python and Django Website using MySQL hosted at PythonAnywhere serving a Blog
-date: 2026-02-23
+date: 2026-02-26
 published: true
 categories:
   - Python
